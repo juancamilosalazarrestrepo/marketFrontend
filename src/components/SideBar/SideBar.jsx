9519205de5c72b34500026a0b3003f45f38bc15f
juan salazar <juancamilosalazarrestrepo@gmail.com>
2023-04-12 18:29:37 -0500

    sidebar fix

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import logo from '../../assets/brand/logoarcadiablanco.png'
 import ComputerIcon from '@mui/icons-material/Computer'
 import AddToQueueIcon from '@mui/icons-material/AddToQueue'
-import './SideBar.css'
+import './Sidebar.css'
 
 function SideBar () {
   return (
